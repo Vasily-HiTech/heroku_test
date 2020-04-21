633f30dd9b827d1069332045ee7a83797f017651
Vasily-HiTech <63966743+Vasily-HiTech@users.noreply.github.com>
2020-04-21 21:33:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const config = require('config')
 const app = express()
 
 app.get('/', (req, res) => {
+    console.log("test passed!")
+    
     res.send('welcome to test heroku')
 })
 app.listen(80)
